bcb3179310ee0ec7dde7d4ec66b986b83ff51927
Lester Q9 <87626207+LesterQ9@users.noreply.github.com>
2023-09-11 20:54:17 +0800

    Update mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 bool isWin(char game[3][3]) {
     bool win = false;
-    if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-    if (game[1][0] == game[0][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true;
-    if (game[2][0] == game[0][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true;
+    // row
+    if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) { win = true; }
+    else if (game[1][0] == game[0][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) { win = true; }
+    else if (game[2][0] == game[0][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) { win = true; }
     // column
-    if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-    if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) win = true;
-    if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
+    else if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) { win = true; }
+    else if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) { win = true; }
+    else if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) { win = true; }
     / diagonal
-    if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
-    if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
+    else if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) { win = true; }
+    else if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) { win = true; }
     return win;
 }
 
@@ -25,24 +26,31 @@ int main() {
     cout << "X = Player 1" << endl << "O = Player 2" << endl;
     for (int n = 0; n < 9; n++) {
         turn = !turn;  // use the not-operator to change true to false or false to true.
-        if (turn == false)
+        if (turn == false) {
             cout << "Player 1: ";
-        else
+        }
+        else {
             cout << "Player 2: ";
-        cout << "Which cell to mark? i:[1..3], j:[1..3]: ";
-        cin >> i >> j;
-        if (turn == false)
-            game[i][j] == 'X';
-        else
-            game[i][j] == 'O';
+        }
+        do {
+            cout << "Which cell to mark? i:[1..3], j:[1..3]: ";
+            cin >> i >> j;
+        } while (i > 3 || i < 1 || j > 3 || j < 1};
+        if (turn == false) {
+            game[i - 1][j - 1] == 'X';
+        }
+        else {
+            game[i - 1][j - 1] == 'O';
+        }
         if (isWin(game)) {
             cout << "Win!" << endl;
             break; // need to terminate the problem
         }
     }
-    if (i==3)  // all cells have been inputted above but no winner yet
+    if (n == 9) {  // all cells have been inputted above but no winner yet
         cout << "Tie!" << endl;
-
+    }
+        
     //show the game console
     cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl;
     cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << endl;
